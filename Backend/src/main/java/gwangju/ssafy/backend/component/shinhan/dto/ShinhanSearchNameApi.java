1d9c622d2f64599d9015ac6d1fff8ae7628cd3a0
Dohun Lee <nestour95@gmail.com>
2023-09-02 23:09:07 +0900

    feat: 신한 예금주 실명 조회 기능 구현 (#11)
    * feat: 예금주 실명 조회 DTO 작성
    
    * feat: 예금주 실명 조회 기능 구현

---
+++ Backend/src/main/java/gwangju/ssafy/backend/component/shinhan/dto/ShinhanSearchNameApi.java
@@ -0,0 +1,35 @@
+package gwangju.ssafy.backend.component.shinhan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ShinhanSearchNameApi {
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class Request {
+		private String 입금은행코드;
+		private String 입금계좌번호;
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Response {
+		private String 입금은행코드;
+		private String 입금계좌번호;
+		private String 입금계좌성명;
+
+		public DepositHolderName toDto(){
+			return DepositHolderName.builder()
+				.backCode(this.입금은행코드)
+				.accountNumber(this.입금계좌번호)
+				.name(this.입금계좌성명)
+				.build();
+		}
+	}
+}
