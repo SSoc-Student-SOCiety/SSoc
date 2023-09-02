befd3a90fba9107a7eb73eca842d1b361a384ff3
Dohun Lee <nestour95@gmail.com>
2023-09-02 20:14:56 +0900

    feat: 신한 계좌 내역 조회 API 용 DTO 작성

---
+++ Backend/src/main/java/gwangju/ssafy/backend/component/shinhan/dto/ShinhanResponseHeader.java
@@ -0,0 +1,13 @@
+package gwangju.ssafy.backend.component.shinhan.dto;
+
+import lombok.Data;
+
+@Data
+public class ShinhanResponseHeader {
+
+	private String successCode;
+	private String resultCode;
+	private String resultMessage;
+
+
+}
