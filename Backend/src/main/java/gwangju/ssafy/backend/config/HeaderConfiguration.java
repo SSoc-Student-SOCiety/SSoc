fab8b6392bf12d693db553c9da6b2f081360247a
Dohun Lee <nestour95@gmail.com>
2023-09-02 20:16:59 +0900

    feat: 신한 계좌 내역 조회 FeignClient 작성

---
+++ Backend/src/main/java/gwangju/ssafy/backend/config/HeaderConfiguration.java
@@ -0,0 +1,14 @@
+package gwangju.ssafy.backend.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+
+public class HeaderConfiguration {
+
+	@Bean
+	public RequestInterceptor requestInterceptor() {
+		return template -> {
+			template.header("Content-Type", "application/json;charset=utf-8");
+		};
+	}
+}
