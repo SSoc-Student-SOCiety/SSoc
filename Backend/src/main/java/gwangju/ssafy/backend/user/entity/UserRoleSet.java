6b3d5eeadc866fe28289423c8cb03738768e66db
동긔 <tlsehdrms124@naver.com>
2023-09-03 15:23:28 +0900

    feat: User 및 User 권한 엔티티 작성

---
+++ Backend/src/main/java/gwangju/ssafy/backend/user/entity/UserRoleSet.java
@@ -0,0 +1,17 @@
+package gwangju.ssafy.backend.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class UserRoleSet {
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "authority")
+    private String authority;   // 유저 권한
+}
