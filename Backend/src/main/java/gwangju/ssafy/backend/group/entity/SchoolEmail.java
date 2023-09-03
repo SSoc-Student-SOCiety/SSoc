fc34467f9e297964081c55ff054b47a87562ec30
Dohun Lee <nestour95@gmail.com>
2023-09-03 12:04:37 +0900

    feat: SchoolEmail 엔티티 작성

---
+++ Backend/src/main/java/gwangju/ssafy/backend/group/entity/SchoolEmail.java
@@ -0,0 +1,37 @@
+package gwangju.ssafy.backend.group.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "school_email")
+@Entity
+public class SchoolEmail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(nullable = false)
+	private School school;
+
+	@Column(nullable = false)
+	private String email;
+
+}
