71de8e37caa3e843742c08b896b2f10d9d1d33c6
동긔 <tlsehdrms124@naver.com>
2023-09-03 15:29:56 +0900

    feat: 회원정보 관련 중간 단계 개발
    1. 구글메일을 이용한 메일 전송 기능 개발
    2. 회원정보 테이블에서 메일 중복 체크 기능 개발
    3. BadRequset Exception 발생시 기능 개발
    4. 메일관련 내용을 담을 MailDto 구현
    5. UserRepository 생성
    6. UserService 및 UserServiceImpl 생성 및 개발
    7. 해당 테스트 파일 개발

---
+++ Backend/src/main/java/gwangju/ssafy/backend/user/service/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package gwangju.ssafy.backend.user.service;
+
+import gwangju.ssafy.backend.user.dto.MailDto;
+import gwangju.ssafy.backend.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+@Transactional  // DB 관련 모든 작업은 트랜잭션 안에서 수행되어야 한다
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    private final JavaMailSender emailSender;
+
+    // 이메일 보내기
+    @Override
+    public void sendSimpleMessage(MailDto mailDto) {
+        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom("tlsehdrms124@yonsei.ac.kr"); // 구글 정책 때문에 발신자(보내는사람) 세팅 안됨
+
+        message.setTo(mailDto.getAddress());    // 수신자(받는사람) 이메일 주소 세팅
+        message.setSubject(mailDto.getTitle()); // 이메일 제목 세팅
+        message.setText(mailDto.getContent());  // 이메일 내용 세팅
+        emailSender.send(message);  // 이메일 보내기
+    }
+
+    // 이에일 중복 체크를 위해 이메일이 존재하는지 여부 조회
+    @Transactional
+    @Override
+    public boolean existsUserByUserEmail(String userEmail) {
+        return userRepository.existsUserByUserEmail(userEmail);
+    }
+
+
+    // 학교이름 - 이메일 해시맵 이용해서 테스트
+    @Override
+    public void schoolEmailTest(String schoolName, String schoolEmail) {
+        HashMap<String, String> schoolEmailHashMap = new HashMap<>();
+
+        schoolEmailHashMap.put(schoolName, schoolEmail);
+    }
+
+    @Override
+    public boolean checkSchoolEmail(String email) {
+        return userRepository.checkSchoolEmail(email);
+    }
+
+
+}
---
+++ Backend/src/test/java/gwangju/ssafy/backend/user/repository/UserRepositoryTest.java
@@ -0,0 +1,63 @@
+package gwangju.ssafy.backend.user.repository;
+
+import gwangju.ssafy.backend.user.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("유저 생성 - 성공")
+    void save() {
+        // given
+        User user = User.builder()
+                .userEmail("test1234@yonsei.ac.kr")
+                .userPassword("1234")
+                .userName("테스트1")
+                .userNickNmae("테스트 닉네임")
+                .userCreateDate(LocalDateTime.now())
+                .build();
+        // when
+        User saveUser = userRepository.save(user);
+
+        // then
+        Assertions.assertEquals("test1234@yonsei.ac.kr", saveUser.getUserEmail());
+        Assertions.assertEquals("테스트1", saveUser.getUserName());
+
+        System.out.println(saveUser.getUserEmail());
+    }
+
+    @Test
+    @DisplayName("유저 이메일 중복 체크 - 성공")
+    void emailCheck() {
+        // given
+        User user = User.builder()
+                .userEmail("test1234@yonsei.ac.kr")
+                .userPassword("1234")
+                .userName("테스트1")
+                .userNickNmae("테스트 닉네임")
+                .userCreateDate(LocalDateTime.now())
+                .build();
+        // when
+        User saveUser = userRepository.save(user);
+
+        // then
+        System.out.println(userRepository.existsUserByUserEmail("test1234@yonsei.ac.kr"));
+
+        System.out.println(saveUser.getUserEmail());
+    }
+
+
+}
