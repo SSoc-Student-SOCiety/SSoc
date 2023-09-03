b08a804adb10e9abaf6a9d036c99aa9f329fdbb2
Dohun Lee <nestour95@gmail.com>
2023-09-03 12:04:55 +0900

    feat: Group 엔티티 작성

---
+++ Backend/src/main/java/gwangju/ssafy/backend/group/entity/Group.java
@@ -0,0 +1,47 @@
+package gwangju.ssafy.backend.group.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Group {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(nullable = false)
+	private School school;
+
+	@Column
+	private String name;
+
+	@Column
+	private String category;
+
+	@Column
+	private String aboutUs;
+
+	@Lob
+	private String introduce;
+
+}
