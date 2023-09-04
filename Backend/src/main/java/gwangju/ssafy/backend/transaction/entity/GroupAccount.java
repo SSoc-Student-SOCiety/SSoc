f93cd0a741608411b73dc16dc5536d625711ab3a
Dohun Lee <nestour95@gmail.com>
2023-09-04 20:09:02 +0900

    feat: 그룹 계좌 엔티티 및 레포지토리 작성

---
+++ Backend/src/main/java/gwangju/ssafy/backend/transaction/entity/GroupAccount.java
@@ -0,0 +1,33 @@
+package gwangju.ssafy.backend.transaction.entity;
+
+import gwangju.ssafy.backend.group.entity.Group;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class GroupAccount {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	private Group group;
+
+	@Column
+	private String number;
+
+}
