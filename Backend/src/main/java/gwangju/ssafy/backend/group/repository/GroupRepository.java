391cf7117644049a77b77e5fff75c97311b5b9a5
LeeDohun <nestour95@gmail.com>
2023-09-04 13:44:04 +0900

    feat: 그룹관련 기반 레포지토리 추가
    - 그룹원, 그룹, 학교 레포지토리 추가

---
+++ Backend/src/main/java/gwangju/ssafy/backend/group/repository/GroupRepository.java
@@ -0,0 +1,13 @@
+package gwangju.ssafy.backend.group.repository;
+
+import gwangju.ssafy.backend.group.entity.Group;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+
+	Optional<Group> findByIdAndActiveIsTrue(Long id);
+
+}
---
+++ Backend/src/main/java/gwangju/ssafy/backend/group/repository/SchoolRepository.java
@@ -0,0 +1,9 @@
+package gwangju.ssafy.backend.group.repository;
+
+import gwangju.ssafy.backend.group.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
+}
