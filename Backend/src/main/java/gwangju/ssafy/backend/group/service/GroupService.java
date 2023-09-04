144873538bab252732459301b6b4f41b974951e1
Dohun Lee <nestour95@gmail.com>
2023-09-04 15:11:33 +0900

    feat: 그룹 생성 API 구현 (#29)
    * feat: 그룹원 엔티티 추가 및 그룹 엔티티 수정
    
    - 그룹 엔티티는 활성화 컬럼을 추가
    
    * feat: 그룹관련 기반 레포지토리 추가
    
    - 그룹원, 그룹, 학교 레포지토리 추가
    
    * feat: 그룹원 추가 기능 구현
    
    * feat: 그룹 생성 기능 구현
    
    - 그룹 생성 시, 관리자 소속원 추가
    
    * feat: 범용 응답 클래스 작성
    
    - 컨트롤러에서 응답 값을 클라이언트로 돌려줄 때 사용할 데이터 래퍼 클래스 작성
    
    * feat: 그룹 생성 API 구현
    
    - 스프링 시큐리티 적용 전이라 권한 체크 X
    
    * fix: Group 테이블명 예약어 사용으로 인한 수정
    
    * fix: JPA 쿼리 메소드 오류 수정

---
+++ Backend/src/main/java/gwangju/ssafy/backend/group/service/GroupService.java
@@ -0,0 +1,9 @@
+package gwangju.ssafy.backend.group.service;
+
+import gwangju.ssafy.backend.group.dto.CreateGroupRequest;
+
+public interface GroupService {
+
+	void createGroup(CreateGroupRequest request);
+
+}
