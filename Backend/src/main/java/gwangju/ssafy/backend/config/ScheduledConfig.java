ef2e8ca2af6afc28c0d7671444618d61dfb1eff7
Dohun Lee <nestour95@gmail.com>
2023-09-04 20:10:55 +0900

    feat: 배치 사용을 위한 설정 추가

---
+++ Backend/src/main/java/gwangju/ssafy/backend/config/ScheduledConfig.java
@@ -0,0 +1,10 @@
+package gwangju.ssafy.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class ScheduledConfig {
+
+}
