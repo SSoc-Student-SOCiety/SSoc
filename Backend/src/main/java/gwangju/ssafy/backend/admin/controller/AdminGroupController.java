f8a75696c105277695d1ca653801ce7776c599b2
LeeDohun <nestour95@gmail.com>
2023-09-04 17:17:28 +0900

    feat: 그룹 비활성화 기능 구현

--- Backend/src/main/java/gwangju/ssafy/backend/admin/controller/AdminGroupController.java
+++ Backend/src/main/java/gwangju/ssafy/backend/admin/controller/AdminGroupController.java
@@ -5,7 +5,11 @@ import gwangju.ssafy.backend.group.dto.CreateGroupRequest;
 import gwangju.ssafy.backend.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +21,15 @@ public class AdminGroupController {
 
 	private final GroupService groupService;
 
-	@PostMapping("/group")
+	@PostMapping("/groups")
 	public ResponseEntity<Message> createGroup(@RequestBody CreateGroupRequest request) {
 		groupService.createGroup(request);
 		return ResponseEntity.ok().body(Message.success());
 	}
 
-
-
+	@GetMapping("/groups/{id}/inactive")
+	public ResponseEntity<Message> inactivateGroup(@PathVariable Long id) {
+		groupService.inactiveGroup(id);
+		return ResponseEntity.ok().body(Message.success());
+	}
 }
