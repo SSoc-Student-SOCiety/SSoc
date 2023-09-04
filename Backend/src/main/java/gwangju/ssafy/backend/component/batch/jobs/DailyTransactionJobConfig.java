9cf76041291eee42bd9bd30f5ad3a4a57373887f
Dohun Lee <nestour95@gmail.com>
2023-09-04 20:11:43 +0900

    feat: 매일 거래내역 결산 배치 기능 구현
    - 스프링 배치 및 스케줄러를 이용해서 매일 정각 2시에 업로드 되지 않은 거래내역 업로드

---
+++ Backend/src/main/java/gwangju/ssafy/backend/component/batch/jobs/DailyTransactionJobConfig.java
@@ -0,0 +1,64 @@
+package gwangju.ssafy.backend.component.batch.jobs;
+
+import gwangju.ssafy.backend.component.shinhan.ShinhanBankService;
+import gwangju.ssafy.backend.transaction.entity.Transaction;
+import gwangju.ssafy.backend.transaction.repository.GroupAccountRepository;
+import gwangju.ssafy.backend.transaction.repository.TransactionRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class DailyTransactionJobConfig {
+
+	private final ShinhanBankService shinhanBankService;
+	private final GroupAccountRepository groupAccountRepository;
+	private final TransactionRepository transactionRepository;
+
+	@Bean
+	public Job dailyTransactionJob(JobRepository jobRepository,
+		PlatformTransactionManager platformTransactionManager) {
+		return new JobBuilder("daily_transaction", jobRepository)
+			.start(dailyTransactionStep(jobRepository, platformTransactionManager))
+			.build();
+	}
+
+	@Bean
+	@JobScope
+	public Step dailyTransactionStep(JobRepository jobRepository,
+		PlatformTransactionManager transactionManager) {
+		return new StepBuilder("daily_transaction", jobRepository)
+			.<List<Transaction>, List<Transaction>>chunk(1, transactionManager)
+			.reader(transactionInfoReader())
+			.writer(transactionWriter())
+			.build();
+	}
+
+	@Bean
+	@StepScope
+	public ItemReader<List<Transaction>> transactionInfoReader() {
+		return new DailyTransactionReader(shinhanBankService, groupAccountRepository,
+			transactionRepository);
+	}
+
+	@Bean
+	@StepScope
+	public ItemWriter<List<Transaction>> transactionWriter() {
+		return new DailyTransactionWriter(transactionRepository);
+	}
+
+}
