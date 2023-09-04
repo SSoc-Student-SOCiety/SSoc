5a7685266711b86afd81789c2f3a8d155c30975d
LeeDohun <nestour95@gmail.com>
2023-09-04 15:04:11 +0900

    fix: Group 테이블명 예약어 사용으로 인한 수정

--- Backend/src/main/java/gwangju/ssafy/backend/group/entity/Group.java
+++ Backend/src/main/java/gwangju/ssafy/backend/group/entity/Group.java
@@ -9,8 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "Groups")
 @Entity
 public class Group {
 
