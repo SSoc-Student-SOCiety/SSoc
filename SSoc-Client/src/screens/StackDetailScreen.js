30d8aad74ca2bdbab6993640780e30c216543e40
hanju <rondo2860@gmail.com>
2023-09-04 14:51:28 +0900

    feat: Main Bottom tab에서 사용할 스택 네비게이션 구현

---
+++ SSoc-Client/src/screens/StackDetailScreen.js
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+import {StackHeader} from "../modules/StackHeader"
+export const StackDetailScreen =()=>{
+
+    return (
+        <View style={{flex:1}}>
+            <StackHeader title={"stack test"}/>
+        </View>
+    )
+}
