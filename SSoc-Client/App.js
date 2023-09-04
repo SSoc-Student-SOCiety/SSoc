df791437a4e49f8bb2f3a79891d86632b968c809
hanju <rondo2860@gmail.com>
2023-09-04 14:57:51 +0900

    feat: App.js에 네비게이션 적용

--- SSoc-Client/App.js
+++ SSoc-Client/App.js
@@ -1,10 +1,13 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RootApp } from "./src/RootApp";
-
+import { SettingScreen } from "./src/screens/MainBottomTabs/SettingScreen";
+import {SearchResultScreen} from "./src/screens/MainBottomTabs/SearchResultScreen"
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
     <SafeAreaProvider>
-      <RootApp />
+      <NavigationContainer><RootApp/></NavigationContainer>
+       
     </SafeAreaProvider>
   );
 }
