72822d925bd87bca28022449693436f0b881b791
hanju <rondo2860@gmail.com>
2023-09-05 17:03:49 +0900

    refator: Main Screen 내부 컴포넌트 분할 및 각 컴포넌트 리팩토링(#40)
    - main screen : mock 데이터 변수 분할 및 반복문으로 렌더링 처리
    - main screen : 스타일 시트 이름 변경
    - stackheader : 세팅 스크린으로 정상 이동
    - profile image : 의미 없는 console.log 삭제
    - main search bar : search button 컴포넌트 분할 적용

--- SSoc-Client/src/screens/MainBottomTabs/MainScreen.js
+++ SSoc-Client/src/screens/MainBottomTabs/MainScreen.js
@@ -1,74 +1,80 @@
-import { View, ScrollView, StyleSheet } from "react-native/";
+import React from "react";
+import { View, ScrollView, StyleSheet } from "react-native";
 import { MainHeader } from "../../modules/MainHeader";
 import { Logo } from "../../modules/Logo";
 import { MainSearchBar } from "../../modules/MainSerachBar";
-import { Typography } from "../../components/Basic/Typography";
-import { Spacer } from "../../components/Basic/Spacer";
 import { Card } from "../../modules/Card";
-import { Button } from "../../components/Basic/Button";
 import { useNavigation } from "@react-navigation/native";
 
 export const MainScreen = () => {
-
   const navigation = useNavigation();
-  const onPressDetail =()=>{
+
+  const onPressDetail = () => {
     console.log("test");
     navigation.navigate("StackDetailScreen");
-  }
+  };
 
   return (
-    <View style={{ flex: 1  }} backgroundColor={"white"}>
+    <View style={styles.container}>
       <MainHeader name={"김싸피"} url={"https://picsum.photos/600"} />
       <Logo />
       <MainSearchBar />
-      <View style={styles.mainItem}>
-        <Typography fontSize={12}>자주 방문한 동아리 결산 보러가기</Typography>
-        <Spacer space={4} />
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <Button onPress={onPressDetail}>
-            <Card
-              url={"https://picsum.photos/600"}
-              schoolName={"한국대학교"}
-              groupName={"정오"}
-              groupType={"총학생회"}
-              group={"제36대 총학생회"}
-              val={1}
-              unit={"days"}
-            />
-          </Button>
-          <Card
-            url={"https://picsum.photos/700"}
-            schoolName={"한국대학교"}
-            groupName={"정일"}
-            groupType={"동아리"}
-            group={"중앙 밴드동아리"}
-            val={1}
-            unit={"days"}
-          />
-          <Card
-            url={"https://picsum.photos/500"}
-            schoolName={"한국대학교"}
-            groupName={"정일"}
-            groupType={"동아리"}
-            group={"중앙 밴드동아리"}
-            val={1}
-            unit={"days"}
-          />
-          <Card
-            url={"https://picsum.photos/300"}
-            schoolName={"한국대학교"}
-            groupName={"정일"}
-            groupType={"동아리"}
-            group={"중앙 밴드동아리"}
-            val={1}
-            unit={"days"}
-          />
-        </ScrollView>
-      </View>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.cardContainer}
+      >
+        {
+          cardsData.map((card, index) => (
+            <Card key={index} {...card} onPress={onPressDetail} />
+          ))
+        }
+      </ScrollView>
     </View>
   );
 };
-var styles = StyleSheet.create({
-  mainItem: { paddingTop: 60, paddingHorizontal: 20 },
-  searchBar: { paddingTop: 30, paddingHorizontal: 20 },
+
+
+  const cardsData = [
+    {
+      url: "https://picsum.photos/600",
+      schoolName: "한국대학교",
+      groupName: "정오",
+      groupType: "총학생회",
+      group: "제36대 총학생회",
+      val: "1",
+      unit: "days",
+    },
+    {
+      url: "https://picsum.photos/700",
+      schoolName: "한국대학교",
+      groupName: "정일",
+      groupType: "동아리",
+      group: "중앙 밴드동아리",
+      val: "1",
+      unit: "days",
+    },
+    {
+      url: "https://picsum.photos/500",
+      schoolName: "한국대학교",
+      groupName: "정일",
+      groupType: "동아리",
+      group: "중앙 밴드동아리",
+      val: "1",
+      unit: "days",
+    },
+    {
+      url: "https://picsum.photos/300",
+      schoolName: "한국대학교",
+      groupName: "정일",
+      groupType: "동아리",
+      group: "중앙 밴드동아리",
+      val: "1",
+      unit: "days",
+    },
+  ]
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "white" },
+  cardContainer: { paddingTop: 60, paddingHorizontal: 20 },
 });
