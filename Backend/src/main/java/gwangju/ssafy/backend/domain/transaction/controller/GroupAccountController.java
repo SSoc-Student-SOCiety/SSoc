688c20b4827d56fc2fd29f1319f037738d371754
LeeDohun <nestour95@gmail.com>
2023-09-06 13:49:37 +0900

    feat: 그룹 계좌 연동 API 작성

---
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/transaction/controller/GroupAccountController.java
@@ -0,0 +1,36 @@
+package gwangju.ssafy.backend.domain.transaction.controller;
+
+import gwangju.ssafy.backend.domain.group.dto.EditGroupInfoRequest;
+import gwangju.ssafy.backend.domain.transaction.dto.RegisterGroupAccountRequest;
+import gwangju.ssafy.backend.domain.transaction.dto.SendAuthCodeRequest;
+import gwangju.ssafy.backend.domain.transaction.service.GroupAccountService;
+import gwangju.ssafy.backend.global.common.dto.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/group/account")
+@RestController
+public class GroupAccountController {
+
+	private final GroupAccountService groupAccountService;
+
+	@PostMapping("/auth/send")
+	public ResponseEntity<Message> sendAuthCode(@RequestBody SendAuthCodeRequest request) {
+		groupAccountService.sendAuthCode(request);
+		return ResponseEntity.ok().body(Message.success());
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<Message> sendAuthCode(@RequestBody RegisterGroupAccountRequest request) {
+		groupAccountService.registerGroupAccount(request);
+		return ResponseEntity.ok().body(Message.success());
+	}
+
+
+
+}
