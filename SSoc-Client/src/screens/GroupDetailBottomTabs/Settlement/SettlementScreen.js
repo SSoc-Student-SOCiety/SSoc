42d1f36f4c335c0e4ee05625119019a4ab47b94b
dev1week <rondo2860@gmail.com>
2023-09-08 01:59:15 +0900

    feat: group detail bottomtab에 사용할 screen 정의(#61)

---
+++ SSoc-Client/src/screens/GroupDetailBottomTabs/Settlement/SettlementScreen.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Typography } from "../../../components/Basic/Typography";
+import { StackHeader } from "../../../modules/StackHeader";
+export const SettlementScreen = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <StackHeader title="결산안" />
+    </View>
+  );
+};
