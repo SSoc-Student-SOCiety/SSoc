94e5a689c801b319eabd43cb578c87fc071b1e33
Dohun Lee <nestour95@gmail.com>
2023-09-08 00:08:25 +0900

    feat: 가입 그룹 조회 API 구현 (#63)

---
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/group/dto/MyGroupSearchCond.java
@@ -0,0 +1,30 @@
+package gwangju.ssafy.backend.domain.group.dto;
+
+import gwangju.ssafy.backend.domain.group.entity.enums.GroupCategory;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyGroupSearchCond {
+
+	private String word;
+	private GroupCategory category;
+	private Long userId;
+
+	@NotNull
+	@Min(1)
+	@Max(100)
+	private Long limit;
+	private long pageNumber;
+}
--- Backend/src/main/java/gwangju/ssafy/backend/domain/group/repository/QueryDslGroupRepository.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/group/repository/QueryDslGroupRepository.java
@@ -2,11 +2,13 @@ package gwangju.ssafy.backend.domain.group.repository;
 
 import gwangju.ssafy.backend.domain.group.dto.GroupSearchCond;
 import gwangju.ssafy.backend.domain.group.dto.GroupSimpleInfo;
+import gwangju.ssafy.backend.domain.group.dto.MyGroupSearchCond;
 import java.util.List;
 
 public interface QueryDslGroupRepository {
 
 	// 그룹 필터링 및 페이징 조회
-	public List<GroupSimpleInfo> findAllBySearchCond(GroupSearchCond cond);
+	List<GroupSimpleInfo> findAllBySearchCond(GroupSearchCond cond);
+	List<GroupSimpleInfo> findMyGroups(MyGroupSearchCond cond);
 
 }
