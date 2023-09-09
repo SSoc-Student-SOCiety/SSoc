596a4cdc74f12a376588e89ce0839e7c4caa7a23
김수린 <uhyeon1009@gmail.com>
2023-09-10 02:04:16 +0900

    feat: LoginScreen 내 api 더미데이터 선작업 완료

--- SSoc-Client/src/screens/AuthScreen/LoginScreen.js
+++ SSoc-Client/src/screens/AuthScreen/LoginScreen.js
@@ -11,12 +11,14 @@ import { useNavigation } from '@react-navigation/native'
 import { getLoginDataFetch } from '../../util/FetchUtil'
 import { useRecoilState } from 'recoil'
 import { goMainPageState, UserInfoState } from '../../util/RecoilUtil/Atoms'
+import { AuthStackHeader } from '../../modules/AuthModules/AuthStackHeader'
+import { setTokens } from '../../util/TokenUtil'
 
 const LoginScreen = (props) => {
   const navigation = useNavigation()
 
-  const [userId, setUserId] = useState('')
-  const [userPw, setUserPw] = useState('')
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassWord, setUserPassWord] = useState('')
 
   const [authInfo, setAuthInfo] = useState(null)
   const [user, setUser] = useRecoilState(UserInfoState)
@@ -25,7 +27,7 @@ const LoginScreen = (props) => {
   const tempData = {
     dataHeader: {
       successCode: 0,
-      resultCode: 0,
+      resultCode: 1,
       resultMessage: null,
     },
     dataBody: {
@@ -38,7 +40,7 @@ const LoginScreen = (props) => {
       token: {
         grantType: 'bearer',
         accessToken: 'testSplashScreen_REFLASH_AccessToken',
-        refreshToken: 'testSplashScreen_REFLASH_RefleshToken',
+        refleshToken: 'testSplashScreen_REFLASH_RefleshToken',
         accessTokenExpiresIn: 12345153351,
       },
     },
@@ -46,7 +48,7 @@ const LoginScreen = (props) => {
 
   const getLoginData = async () => {
     try {
-      const response = await getLoginDataFetch()
+      const response = await getLoginDataFetch(userEmail, userPassWord)
       const data = await response.json()
       await setAuthInfo(tempData)
     } catch (e) {
@@ -54,112 +56,146 @@ const LoginScreen = (props) => {
     }
   }
 
+  const onPressFindPw = () => {
+    navigation.navigate('FindPassWord')
+  }
+
   const onPressRegister = () => {
     navigation.reset({ routes: [{ name: 'SchoolEmail' }] })
   }
 
   const onPressLogin = () => {
-    if (userId.length == 0) {
+    if (userEmail.length == 0) {
       Alert.alert('아이디를 입력해주세요.')
-    } else if (userPw.length < 8) {
+    } else if (userPassWord.length < 8) {
       Alert.alert('비밀번호는 8자 이하일 수 잆습니다.')
     } else {
-      getLoginData()
+      getLoginData(userEmail, userPassWord)
     }
   }
 
   useEffect(() => {
     if (authInfo != null) {
       if (authInfo.dataHeader.successCode == 0) {
+        if (authInfo.dataHeader.resultCode == 1) {
+          setTokens(authInfo.dataBody.token.accessToken, authInfo.dataBody.token.refleshToken)
+        }
         setUser(authInfo.dataBody.userInfo)
         setGoMainPage(true)
       } else {
         Alert.alert('아이디 또는 비밀번호를 확인해주세요.')
       }
+      setAuthInfo(null)
     }
   }, [authInfo])
 
   return (
-    <KeyboardAwareScrollView
+    <View
       backgroundColor={Color.WHITE}
       style={{ flex: 1, alignContent: 'center' }}
     >
-      <Spacer space={16} />
-      <Logo />
-      <Spacer space={10} />
-      <View style={{ margin: 20 }}>
-        {/* 입력 폼 */}
-        <AuthInput
-          fontSize={16}
-          title="학교 이메일"
-          placeholder="ex) test@shinhan.ac.kr"
-          inputColor={Color.BLUE}
-          onChangeText={(text) => {
-            setUserId(text)
-          }}
-        />
-        <Spacer space={12} />
-        <AuthInput
-          fontSize={16}
-          title="비밀번호"
-          inputColor={Color.BLUE}
-          secureTextEntry={true}
-          onChangeText={(text) => {
-            setUserPw(text)
-          }}
-        />
-        <Spacer space={16} />
+      <AuthStackHeader title="로그인" />
+      <KeyboardAwareScrollView>
+        <Logo />
+        <Spacer space={10} />
+        <View style={{ margin: 20 }}>
+          {/* 입력 폼 */}
+          <AuthInput
+            fontSize={16}
+            title="학교 이메일"
+            placeholder="ex) test@shinhan.ac.kr"
+            inputColor={Color.BLUE}
+            onChangeText={(text) => {
+              setUserEmail(text)
+            }}
+          />
+          <Spacer space={12} />
+          <AuthInput
+            fontSize={16}
+            title="비밀번호"
+            inputColor={Color.BLUE}
+            secureTextEntry={true}
+            onChangeText={(text) => {
+              setUserPassWord(text)
+            }}
+          />
+          <Spacer space={24} />
 
-        {/* 로그인 버튼 */}
-        <Button>
-          <TouchableOpacity onPress={onPressLogin}>
-            <View
-              backgroundColor={Color.DARK_BLUE}
-              borderRadius={10}
-              style={{
-                alignSelf: 'stretch',
-                height: 50,
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'row',
-              }}
-            >
-              <Typography
-                fontSize={16}
-                color={Color.WHITE}
-              >
-                로그인
-              </Typography>
-              <Spacer space={10} />
-            </View>
-          </TouchableOpacity>
-        </Button>
-        <Spacer space={14} />
-
-        {/* 회원가입 하러가기 */}
-        <View style={{ flex: 1, alignItems: 'flex-end' }}>
-          <Typography
-            fontSize={14}
-            color={Color.GRAY}
-          >
-            <Text>아직 회원이 아니신가요?</Text>
-          </Typography>
-          <Spacer space={5} />
+          {/* 로그인 버튼 */}
           <Button>
-            <TouchableOpacity onPress={onPressRegister}>
-              <View>
+            <TouchableOpacity onPress={onPressLogin}>
+              <View
+                backgroundColor={Color.DARK_BLUE}
+                borderRadius={10}
+                style={{
+                  alignSelf: 'stretch',
+                  height: 50,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  flexDirection: 'row',
+                }}
+              >
                 <Typography
-                  fontSize={15}
-                  color={Color.DARK_BLUE}
+                  fontSize={16}
+                  color={Color.WHITE}
                 >
-                  <Text style={{ fontWeight: 'bold', textDecorationLine: 'underline' }}>회원가입 하러가기</Text>
+                  로그인
                 </Typography>
+                <Spacer space={10} />
               </View>
             </TouchableOpacity>
           </Button>
+          <Spacer space={14} />
+
+          {/* 회원가입 하러가기 */}
+          <View style={{ marginTop: 30, flex: 1, flexDirection: 'row', paddingHorizontal: 10 }}>
+            <View>
+              <Typography
+                fontSize={14}
+                color={Color.GRAY}
+              >
+                <Text>비밀번호를 잊으셨나요?</Text>
+              </Typography>
+              <Spacer space={5} />
+              <Button>
+                <TouchableOpacity onPress={onPressFindPw}>
+                  <View>
+                    <Typography
+                      fontSize={15}
+                      color={Color.DARK_BLUE}
+                    >
+                      <Text style={{ fontWeight: 'bold', textDecorationLine: 'underline' }}>비밀번호 찾기</Text>
+                    </Typography>
+                  </View>
+                </TouchableOpacity>
+              </Button>
+            </View>
+
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <Typography
+                fontSize={14}
+                color={Color.GRAY}
+              >
+                <Text>아직 회원이 아니신가요?</Text>
+              </Typography>
+              <Spacer space={5} />
+              <Button>
+                <TouchableOpacity onPress={onPressRegister}>
+                  <View>
+                    <Typography
+                      fontSize={15}
+                      color={Color.DARK_BLUE}
+                    >
+                      <Text style={{ fontWeight: 'bold', textDecorationLine: 'underline' }}>회원가입 하러가기</Text>
+                    </Typography>
+                  </View>
+                </TouchableOpacity>
+              </Button>
+            </View>
+          </View>
         </View>
-      </View>
-    </KeyboardAwareScrollView>
+      </KeyboardAwareScrollView>
+    </View>
   )
 }
 export default LoginScreen
