9c3a5b4eec80d49b45b2499cbe5d336ef3b9cd97
김수린 <uhyeon1009@gmail.com>
2023-09-10 02:02:54 +0900

    feat: SchoolEmailInput 내 api 더미데이터 선작업 완료

--- SSoc-Client/src/modules/AuthModules/SchoolEmailInput.js
+++ SSoc-Client/src/modules/AuthModules/SchoolEmailInput.js
@@ -1,24 +1,63 @@
+import { useState, useEffect } from 'react'
 import { View, Text, Alert, TouchableOpacity } from 'react-native'
 import { Button } from '../../components/Basic/Button'
 import { Spacer } from '../../components/Basic/Spacer'
 import { Typography } from '../../components/Basic/Typography'
 import * as Color from '../../components/Colors/colors'
+import { getEmailAuthCodeFetch } from '../../util/FetchUtil'
 
 const SchoolEmailInput = (props) => {
-  const email = props.email
+  const userEmail = props.userEmail
+  const [emailAuthCodeData, setEmailAuthCodeData] = useState(null)
+  const [waitTime, setWaitTime] = useState(false)
+
+  const tempData = {
+    dataHeader: {
+      successCode: 0,
+      resultCode: null,
+      resultMessage: null,
+    },
+    dataBody: {
+      userEmail: 'tlsehdrms124@yonsei.ac.kr',
+      emailCode: '5naH20s2',
+    },
+  }
+
   const onPressCheckEmail = () => {
-    // TO-DO
-    // 8줄 email Server로 쏴서 response 받기
-    // API로 메일 인증 버튼 클릭 시 인증번호가 오면 onPressCheck() 아니면 에러
-    let res = 'testAuthCode'
-    if (res != '-1') {
-      Alert.alert('이메일 인증코드가 발송되었습니다.', '이메일을 확인해주세요.')
-      props.onPressCheck()
-      props.setEmailCode(res)
+    if (waitTime == false) {
+      getEmailAuthCodeData()
+      setWaitTime(true)
     } else {
-      Alert.alert('잘못된 이메일입니다.')
+      Alert.alert('아직 3분이 지나지 않았습니다.', '이메일 인증코드 발송은 3분에 한 번 가능합니다.')
     }
   }
+
+  const getEmailAuthCodeData = async () => {
+    try {
+      const response = await getEmailAuthCodeFetch(userEmail)
+      const data = await response.json()
+      await setEmailAuthCodeData(tempData)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    if (emailAuthCodeData != null) {
+      setTimeout(() => {
+        setWaitTime(false)
+      }, 180000)
+      if (emailAuthCodeData.dataHeader.successCode == 0) {
+        Alert.alert('이메일 인증코드가 발송되었습니다.', '이메일을 확인해주세요.')
+        props.onPressCheck()
+        props.setEmailCode(emailAuthCodeData.dataBody.emailCode)
+      } else {
+        Alert.alert('잘못된 이메일입니다.')
+      }
+      setEmailAuthCodeData(null)
+    }
+  }, [emailAuthCodeData])
+
   return (
     <View>
       <Typography
@@ -45,7 +84,7 @@ const SchoolEmailInput = (props) => {
             fontSize={16}
             color={Color.BLACK}
           >
-            {email}
+            {userEmail}
           </Typography>
         </View>
         <View style={{ marginLeft: 10, flex: 1 }}>
