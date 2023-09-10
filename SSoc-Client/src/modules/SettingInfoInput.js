3eded54d0c365f14ae62b33146816be4aa04b1f8
김수린 <uhyeon1009@gmail.com>
2023-09-10 16:41:52 +0900

    feat: 회원정보 수정 중 이미지 수정 제외 api 더미데이터로 선작업 완료

--- SSoc-Client/src/modules/SettingInfoInput.js
+++ SSoc-Client/src/modules/SettingInfoInput.js
@@ -2,17 +2,12 @@ import { View } from 'react-native'
 import { Typography } from '../components/Basic/Typography'
 import { SingleLineInput } from '../components/Input/SingleLineInput'
 import * as Color from '../components/Colors/colors'
-import { Spacer } from '../components/Basic/Spacer'
 import SettingBtn from './SettingButton'
-import { Icon } from '../components/Icons/Icons'
 
 const SettingInfoInput = (props) => {
   return (
     <View style={{ marginLeft: 4 }}>
-      <Typography fontSize={16}>
-        {props.editContent}
-        <Icon />
-      </Typography>
+      <Typography fontSize={16}>{props.editContent}</Typography>
       <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
         <View style={{ backgroundColor: Color.LIGHT_GRAY, borderRadius: 4, width: '70%' }}>
           <SingleLineInput
