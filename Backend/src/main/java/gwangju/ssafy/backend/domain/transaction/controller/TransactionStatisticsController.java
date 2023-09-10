98faed97ec15d33a700f70b6940b13ab77350a15
Dohun Lee <nestour95@gmail.com>
2023-09-10 21:55:36 +0900

    feat: 그룹계좌 거래 내역 월간 통계 조회 API 구현
    - 원하는 년도로 월간 통계 조회

--- Backend/src/main/java/gwangju/ssafy/backend/domain/transaction/controller/TransactionStatisticsController.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/transaction/controller/TransactionStatisticsController.java
@@ -2,7 +2,9 @@ package gwangju.ssafy.backend.domain.transaction.controller;
 
 
 import gwangju.ssafy.backend.domain.transaction.dto.DailyStatisticsInfo;
+import gwangju.ssafy.backend.domain.transaction.dto.MonthlyStatisticsInfo;
 import gwangju.ssafy.backend.domain.transaction.dto.SearchDailyStatisticsRequest;
+import gwangju.ssafy.backend.domain.transaction.dto.SearchMonthlyStatisticsRequest;
 import gwangju.ssafy.backend.domain.transaction.service.TransactionStatisticsService;
 import gwangju.ssafy.backend.global.common.dto.Message;
 import java.util.List;
@@ -26,5 +28,10 @@ public class TransactionStatisticsController {
 			.body(Message.success(transactionStatisticsService.searchDailyStatistics(request)));
 	}
 
+	@PostMapping("/monthly")
+	public ResponseEntity<Message<List<MonthlyStatisticsInfo>>> editNote(@RequestBody SearchMonthlyStatisticsRequest request) {
+		return ResponseEntity.ok()
+			.body(Message.success(transactionStatisticsService.searchMonthlyStatistics(request)));
+	}
 
 }
--- Backend/src/main/java/gwangju/ssafy/backend/domain/transaction/repository/DailyTransactionStatisticsRepository.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/transaction/repository/DailyTransactionStatisticsRepository.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Repository;
 public interface DailyTransactionStatisticsRepository extends
 	JpaRepository<DailyTransactionStatistics, Long> {
 
-	@Query("select s from DailyTransactionStatistics s where s.groupAccountId = :groupAccountId and s.date >= :startDate and s.date <= :endDate")
+	@Query("select s from DailyTransactionStatistics s "
+		+ "where s.groupAccountId = :groupAccountId "
+		+ "and s.date >= :startDate and s.date <= :endDate")
 	List<DailyTransactionStatistics> findAllByDate(@Param("groupAccountId") Long groupAccountId,
 		@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
