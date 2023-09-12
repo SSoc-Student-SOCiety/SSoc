31fab8cedd5f9b90f437113e9c45cf3522d3587f
hanju <rondo2860@gmail.com>
2023-09-12 17:47:28 +0900

    fix: 더 이상 사용하지 않는 로그 제거 및 Stack Header에서 세팅 버튼 삭제(#100)

--- SSoc-Client/src/modules/StackHeader.js
+++ SSoc-Client/src/modules/StackHeader.js
@@ -1,6 +1,7 @@
 import { Header } from "../components/header/Header";
 import React, { useCallback } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 export const StackHeader = (props) => {
   const navigation = useNavigation();
@@ -11,17 +12,14 @@ export const StackHeader = (props) => {
   }, []);
 
   //to-do setting 화면가기 구현하기
-  const onPressSetting = useCallback(() => {
-    navigation.navigate("SettingScreen");
-  }, []);
+
 
   return (
     <Header>
       <Header.Icon iconName="arrow-back" onPress={onPressBack} />
 
       <Header.Title title={props.title}></Header.Title>
-
-      <Header.Icon iconName="ios-settings-outline" onPress={onPressSetting} />
+      <Ionicons name="settings" color="white"/>
     </Header>
   );
 };
