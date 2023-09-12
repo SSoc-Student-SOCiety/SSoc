c9436d6eb4cf5df1547e3fabafad5136edbc6e07
동긔 <tlsehdrms124@naver.com>
2023-09-12 13:53:48 +0900

    fix: 불필요한 파일 삭제
    - GlobalExcpetion으로 모든 Controller의 Exception을 통합 관리하므로 파일 삭제

--- Backend/src/main/java/gwangju/ssafy/backend/global/component/jwt/security/JwtAuthenticationEntryPoint.java
+++ Backend/src/main/java/gwangju/ssafy/backend/global/component/jwt/security/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package gwangju.ssafy.backend.global.component.jwt.security;
 
 import jakarta.servlet.ServletException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,11 +13,15 @@ import java.io.IOException;
 /**
  * 자격 증명 없이 접근시, 401 응답을 보여줌
  */
+
+@Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         // 자격증명 없이 접근시, 401응답
+        log.info("====================JwtAuthenticationEntryPoint=====================");
+        log.info("자격 증명 없이 접근한 경우");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
