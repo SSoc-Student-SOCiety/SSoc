e22f2cfab5485ecc28a8ddb7c0382cd70c8ab79a
LeeDohun <nestour95@gmail.com>
2023-09-13 09:14:33 +0900

    hotfix: 게시글 조회에 댓글 수, 유저 아이디, 유저 이미지 추가

--- Backend/src/main/java/gwangju/ssafy/backend/domain/post/dto/PostInfo.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/post/dto/PostInfo.java
@@ -21,10 +21,14 @@ public class PostInfo {
 	private String nickname;
 	private LocalDate createdAt;
 	private String content;
+	// 추가된 부분
+	private Long commentCnt;
+	private String profileImg;
+	private Long userId;
 
 	public PostInfo(Long postId, Long groupId, String title, String nickname,
 		LocalDateTime createdAt,
-		String content) {
+		String content, Long commentCnt, String profileImg, Long userId) {
 		this.postId = postId;
 		this.groupId = groupId;
 		this.title = title;
@@ -32,5 +36,8 @@ public class PostInfo {
 		this.createdAt = LocalDate.of(createdAt.getYear(), createdAt.getMonth(),
 			createdAt.getDayOfMonth());
 		this.content = content;
+		this.commentCnt = commentCnt;
+		this.profileImg = profileImg;
+		this.userId = userId;
 	}
 }
