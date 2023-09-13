10f8626866c9f71d0aa6d3add4fab97b32ebaefb
hanju <rondo2860@gmail.com>
2023-09-13 14:49:07 +0900

    feat: 그룹 멤버 및 요청 관리 네비게이션등록(#116)

---
+++ SSoc-Client/src/screens/GroupDetailBottomTabs/Manage/MemberManage/RequestManageScreen.js
@@ -0,0 +1,10 @@
+import { Typography } from "../../../../components/Basic/Typography"
+import { View } from "react-native"
+
+export const RequestManageScreen = ()=>{
+    return(
+    <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+        <Typography> 신청 및 그룹원 관리 </Typography>
+    </View>
+    )
+}
