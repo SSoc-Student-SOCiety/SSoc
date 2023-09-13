e9cd1af13f7c1cd812633aaf21530e7124475774
동긔 <tlsehdrms124@naver.com>
2023-09-13 22:16:59 +0900

    feat: 해당 그룹 멤버 전체 조회
    - 그룹멤버(소속원)에서 해당 그룹에 속한 멤버 전체를 조회하는 기능 구현 (로그인해서 조회하는 당사자(그룹장)은 제외)

--- Backend/src/main/java/gwangju/ssafy/backend/domain/group/controller/GroupMemberController.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/group/controller/GroupMemberController.java
@@ -1,11 +1,13 @@
 package gwangju.ssafy.backend.domain.group.controller;
 
+import gwangju.ssafy.backend.domain.group.dto.GetGroupMemberInfo;
 import gwangju.ssafy.backend.domain.group.dto.GetMemberRoleRequest;
 import gwangju.ssafy.backend.domain.group.dto.GetMemberRoleResponse;
 import gwangju.ssafy.backend.domain.group.service.GroupMemberService;
 import gwangju.ssafy.backend.domain.user.dto.LoginActiveUserDto;
 import gwangju.ssafy.backend.global.common.dto.Message;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/groups/{groupId}")
 @RestController
@@ -34,4 +39,15 @@ public class GroupMemberController {
 		request.setUserId(login.getId());
 		return ResponseEntity.ok().body(Message.success(groupMemberService.getMemberRole(request)));
 	}
+
+	@PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
+	@GetMapping("/user/list")
+	public ResponseEntity<Message<List<GetGroupMemberInfo>>> getGroupMember(
+			@PathVariable("groupId") Long groupId,
+			@AuthenticationPrincipal LoginActiveUserDto login) {
+		log.info(login.getUserName());
+		List<GetGroupMemberInfo> groupMemberInfoList = groupMemberService.getMemberList(login.getId(), groupId);
+		return ResponseEntity.ok().body(Message.success(groupMemberInfoList));
+	}
+
 }
