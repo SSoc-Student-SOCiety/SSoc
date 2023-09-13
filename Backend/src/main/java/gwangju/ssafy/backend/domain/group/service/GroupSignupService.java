f31b15faf25a7214cbb27caf5a1a405e0094dfaa
동긔 <tlsehdrms124@naver.com>
2023-09-13 15:20:02 +0900

    feat: 그룹 내 가입 신청 처리 조회, 수락, 거절 기능 구현
    - 그룹내에서 가입 신청 처리 대기중인 해당 정보들 조회(리스트화), 해당 가입 신청 수락, 해당 가입 신청 거절 기능을 구현하였습니다.

---
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/group/service/GroupSignupService.java
@@ -0,0 +1,19 @@
+package gwangju.ssafy.backend.domain.group.service;
+
+import gwangju.ssafy.backend.domain.group.dto.DeleteGroupSignInfoRequest;
+import gwangju.ssafy.backend.domain.group.dto.GroupSignupInfo;
+import gwangju.ssafy.backend.domain.group.dto.GetGroupSignupInfo;
+
+import java.util.List;
+
+public interface GroupSignupService {
+    // 그룹 가입에서 해당 그룹에 가입 신청 대기중인 리스트 가져오기
+    List<GroupSignupInfo> searchAllGroupSignup();
+
+    // 그룹에서 해당 유저 가입 거절
+    GroupSignupInfo rejectSignup(DeleteGroupSignInfoRequest request);
+
+    // 그룹 내 해당 가입 신청 수락
+    GroupSignupInfo ApproveGroupSignUp(GetGroupSignupInfo request);
+
+}
