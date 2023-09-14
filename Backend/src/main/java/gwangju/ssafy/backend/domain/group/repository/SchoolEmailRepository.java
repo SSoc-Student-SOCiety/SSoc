2b02d248a46b3b6cbec4f0e621f7ac14cf3c2d8c
LeeDohun <nestour95@gmail.com>
2023-09-14 09:58:47 +0900

    hotfix: Group 엔티티 수정

---
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/group/repository/SchoolEmailRepository.java
@@ -0,0 +1,8 @@
+package gwangju.ssafy.backend.domain.group.repository;
+
+import gwangju.ssafy.backend.domain.group.entity.SchoolEmail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolEmailRepository extends JpaRepository<SchoolEmail, Long> {
+
+}
