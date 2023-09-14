9e15e14f638bd60daf8d9ac1c7acd560bad36ee6
hanju <rondo2860@gmail.com>
2023-09-14 10:53:56 +0900

    feat: 예약 아이템 상세 페이지 예약 가능 시간 조회 구현(#115)

--- SSoc-Client/src/screens/GroupDetailBottomTabs/Booking/BookingItemDetailScreen.js
+++ SSoc-Client/src/screens/GroupDetailBottomTabs/Booking/BookingItemDetailScreen.js
@@ -1,10 +1,12 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, Touchable, TouchableOpacity } from "react-native";
 import { StackHeader } from "../../../modules/StackHeader";
 import * as Color from "../../../components/Colors/colors";
 import { Calendar } from "react-native-calendars";
 import { Text } from "react-native";
 import React, { useEffect, useState } from "react";
-
+import { Typography } from "../../../components/Basic/Typography";
+import { Divider } from "../../../components/Basic/Divider";
+import { Spacer } from "../../../components/Basic/Spacer";
 export const BookingItemDetailScreen = () => {
   const currentDate = new Date(); // 현재 날짜 가져오기
   const year = currentDate.getFullYear();
@@ -13,7 +15,7 @@ export const BookingItemDetailScreen = () => {
   const today = `${year}-${month}-${day}`;
 
   const [timeList, setTimeList] = useState(mockResponse.dataBody[today]);
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState(today);
 
   useEffect(() => {
     // 컴포넌트가 마운트될 때 초기 데이터를 설정합니다.
@@ -36,39 +38,72 @@ export const BookingItemDetailScreen = () => {
   const renderItem = ({ item }) => {
     console.log(item);
     return (
-      <View>
-        <Text>
-          {item.time} {item.available ? 1 : 0}
-        </Text>
-      </View>
+      item.available ?(
+        <TouchableOpacity>
+          <View style={{backgroundColor:Color.LIGHT_BLUE, width: 320, height:40, justifyContent:"space-between", alignItems:"center", margin:5, flexDirection:"row", borderRadius:12}}>
+            <View style={{margin:10}}>
+              <Typography fontSize={15} color={Color.WHITE}> 
+                {item.time}시부터 {item.time+1}시까지 
+              </Typography>
+            </View>
+            <View style={{marginHorizontal:15}}>
+              <Typography color={Color.WHITE}>예약가능</Typography>
+            </View>
+          </View>
+        </TouchableOpacity>
+        ) :(
+        <View style={{backgroundColor:Color.GRAY, width: 320, height:40, justifyContent:"space-between", alignItems:"center", margin:5, flexDirection:"row", borderRadius:12}}>
+          <View style={{margin:10}}>
+            <Typography fontSize={15} color={Color.WHITE}> 
+              {item.time}시부터 {item.time+1}시까지 
+            </Typography>
+          </View>
+          <View style={{marginHorizontal:15}}>
+            <Typography color={Color.WHITE}> 
+              예약 불가
+            </Typography>
+          </View>
+        </View>)
+      
     );
   };
 
   return (
-    <View style={{ backgroundColor: Color.WHITE }}>
+    <View style={{ backgroundColor: Color.WHITE, flex:1 }}>
       <StackHeader title={"예약아이템 상세"} />
-
-      <Calendar
-        onDayPress={(day) => {
-          setSelected(day.dateString);
-          console.log(day);
-        }}
-        markedDates={{
-          [today]: { selected: true, marked: true },
-          [selected]: {
-            selected: true,
-            disableTouchEvent: true,
-            selectedDotColor: Color.RED,
-            color: Color.RED,
-          },
-        }}
-      />
+      <View style={{flex:1}}>
+        <Calendar
+          onDayPress={(day) => {
+            setSelected(day.dateString);
+            console.log(day);
+          }}
+          markedDates={{
+            [selected]: {
+              selected: true,
+              disableTouchEvent: true,
+              selectedDotColor: Color.RED,
+              color: Color.RED,
+            },
+          }}
+        />
+      </View>
+      
       {/* selected 상태가 변경될 때마다 List 컴포넌트를 렌더링 */}
-      <List selectedDate={selected} />
+      <Spacer space={3}/>
+      <Divider/>
+      <Spacer space={3}/>
+      <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+        <List selectedDate={selected} />
+      </View>
+      
     </View>
   );
 };
-
+//실제예약일을 2023-09-14의 형태로 입력을 넘겨줌
+  //예약일을 키값으로 하여 
+    //9~22까지의 time, avaliable을 포함한 객체 배열 넘기기
+  //예약 테이블 내 (반납확인 false && 예약승인여부 true)인 시간을 availavle 값을 true로, 
+    //그외 시간은 전부 false로 보내주기  
 const mockResponse = {
   dataHeader: {
     successCode: 0,
@@ -80,21 +115,21 @@ const mockResponse = {
       { time: 9, available: true },
       { time: 10, available: true },
       { time: 11, available: true },
-      { time: 12, available: true },
+      { time: 12, available: false },
       { time: 13, available: true },
       { time: 14, available: true },
       { time: 15, available: true },
       { time: 16, available: true },
-      { time: 17, available: true },
+      { time: 17, available: false },
       { time: 18, available: true },
       { time: 19, available: true },
-      { time: 20, available: true },
+      { time: 20, available: false },
       { time: 21, available: true },
       { time: 22, available: true },
     ],
     "2023-09-15": [
       { time: 9, available: true },
-      { time: 10, available: true },
+      { time: 10, available: false },
       { time: 11, available: true },
       { time: 12, available: true },
       { time: 13, available: true },
@@ -103,13 +138,13 @@ const mockResponse = {
       { time: 16, available: true },
       { time: 17, available: true },
       { time: 18, available: true },
-      { time: 19, available: true },
+      { time: 19, available: false },
       { time: 20, available: true },
       { time: 21, available: true },
       { time: 22, available: true },
     ],
     "2023-09-16": [
-      { time: 9, available: true },
+      { time: 9, available: false },
       { time: 10, available: true },
       { time: 11, available: true },
       { time: 12, available: true },
