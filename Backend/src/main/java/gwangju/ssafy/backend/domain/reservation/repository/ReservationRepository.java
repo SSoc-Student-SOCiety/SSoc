092c6c6ac77925ec2bd2b277f2b823138fdae65f
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-09-15 21:08:24 +0900

    feat: 그룹 관리자가 예약 내역 조회하는 기능 구현 (#147)
    - 예약 내역 전체 조회 기능 구현
    - 예약 내역 반납 여부에 따른 조회 (미반납 부분)
    - 예약 내역 승인 여부에 따른 조회 (미승인 부분)

--- Backend/src/main/java/gwangju/ssafy/backend/domain/reservation/repository/ReservationRepository.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/reservation/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package gwangju.ssafy.backend.domain.reservation.repository;
 
 import gwangju.ssafy.backend.domain.reservation.entity.Reservation;
+import gwangju.ssafy.backend.domain.reservation.entity.enums.ReservationApproveStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -15,4 +17,14 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @Query("select re from Reservation re where re.product.id = :productId AND to_char(re.realDate, 'yyyy-mm-dd') = :date")
     List<Reservation> findByProductIdAndTime(@Param("productId") Long productId, @Param("date") LocalDate date);
+
+//    @Query("select re from Reservation re where re.product.id = (select pr.id from Product pr where pr.group.id = :groupId)")
+@Query("select r from Reservation r inner join Product p on r.product.id = p.id where p.group.id = :groupId " +
+        "and ((:approveStatus is null) or (r.approveStatus = :approveStatus)) " +
+        "and ((:returnStatus is null) or (r.returnStatus = :returnStatus)) " +
+        "order by r.createdAt asc")
+List<Reservation> findByProductIdReservation(
+        @Param("groupId") Long groupId,
+        @Param("approveStatus") ReservationApproveStatus approveStatus,
+        @Param("returnStatus") Optional<Boolean> returnStatus);
 }
