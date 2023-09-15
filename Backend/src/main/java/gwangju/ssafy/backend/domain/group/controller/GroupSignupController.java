d7f779d3b9403ff7fce6bb541cc7c5a2e867c613
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-09-16 07:51:47 +0900

    feat: 회원이 그룹 가입 신청하는 기능 구현 (#151)

--- Backend/src/main/java/gwangju/ssafy/backend/domain/group/controller/GroupSignupController.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/group/controller/GroupSignupController.java
@@ -4,10 +4,12 @@ import gwangju.ssafy.backend.domain.group.dto.DeleteGroupSignInfoRequest;
 import gwangju.ssafy.backend.domain.group.dto.GetGroupSignupInfo;
 import gwangju.ssafy.backend.domain.group.dto.GroupSignupInfo;
 import gwangju.ssafy.backend.domain.group.service.GroupSignupService;
+import gwangju.ssafy.backend.domain.user.dto.LoginActiveUserDto;
 import gwangju.ssafy.backend.global.common.dto.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,8 +32,9 @@ public class GroupSignupController {
     // 해당 그룹 가입 신청 유저 거절 처리
     @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     @DeleteMapping("/reject/{groupSignupId}")
-    public ResponseEntity<Message<GroupSignupInfo>> rejectSignup(@RequestBody DeleteGroupSignInfoRequest request,
-                                                                      @PathVariable("groupSignupId") Long groupSignupId) {
+    public ResponseEntity<Message<GroupSignupInfo>> rejectSignup(
+            @RequestBody DeleteGroupSignInfoRequest request,
+            @PathVariable("groupSignupId") Long groupSignupId) {
         request.setGroupSignUpId(groupSignupId);
         return ResponseEntity.ok().body(Message.success(groupSignupService.rejectSignup(request)));
     }
@@ -39,9 +42,22 @@ public class GroupSignupController {
     // 해당 그룹 가입 신청 유저 수락 처리
     @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     @PutMapping("/approve/{groupSignupId}")
-    public ResponseEntity<Message<GroupSignupInfo>> approveSignup(@RequestBody GetGroupSignupInfo request,
-                                                                  @PathVariable("groupSignupId") Long groupSignupId) {
+    public ResponseEntity<Message<GroupSignupInfo>> approveSignup(
+            @RequestBody GetGroupSignupInfo request,
+            @PathVariable("groupSignupId") Long groupSignupId) {
         request.setGroupSignUpId(groupSignupId);
-        return ResponseEntity.ok().body(Message.success(groupSignupService.ApproveGroupSignUp(request)));
+        return ResponseEntity.ok().body(Message.success(groupSignupService.ApproveGroupSignup(request)));
     }
+
+    // 유저가 해당 그룹 가입 신청
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
+    @PostMapping("/user/{groupId}")
+    public ResponseEntity<Message<GroupSignupInfo>> requestSignup(
+            @AuthenticationPrincipal LoginActiveUserDto login,
+            @PathVariable("groupId") Long groupId
+    ) {
+        GroupSignupInfo groupSignupInfo = groupSignupService.requestSignup(login.getId(), groupId);
+        return ResponseEntity.ok().body(Message.success(groupSignupInfo));
+    }
+
 }
--- Backend/src/main/java/gwangju/ssafy/backend/domain/group/service/GroupSignupService.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/group/service/GroupSignupService.java
@@ -7,6 +7,7 @@ import gwangju.ssafy.backend.domain.group.dto.GetGroupSignupInfo;
 import java.util.List;
 
 public interface GroupSignupService {
+    // 그룹 관리자 입장
     // 그룹 가입에서 해당 그룹에 가입 신청 대기중인 리스트 가져오기
     List<GroupSignupInfo> searchAllGroupSignup(Long groupId);
 
@@ -14,6 +15,9 @@ public interface GroupSignupService {
     GroupSignupInfo rejectSignup(DeleteGroupSignInfoRequest request);
 
     // 그룹 내 해당 가입 신청 수락
-    GroupSignupInfo ApproveGroupSignUp(GetGroupSignupInfo request);
+    GroupSignupInfo ApproveGroupSignup(GetGroupSignupInfo request);
+
+    // 그룹에 아직 가입되어 있지 않은 유저가 해당 그룹에 가입 신청
+    GroupSignupInfo requestSignup(Long userId, Long groupId);
 
 }
