338008407338dfa492a2a6ca7eb050d75118f650
Dohun Lee <nestour95@gmail.com>
2023-09-16 15:36:06 +0900

    chore: 유저 인증 코드 이메일 템플릿 적용

--- Backend/src/main/java/gwangju/ssafy/backend/global/infra/email/EmailServiceImpl.java
+++ Backend/src/main/java/gwangju/ssafy/backend/global/infra/email/EmailServiceImpl.java
@@ -1,11 +1,13 @@
 package gwangju.ssafy.backend.global.infra.email;
 
 import gwangju.ssafy.backend.global.common.dto.MailCodeDto;
+import gwangju.ssafy.backend.global.component.alarm.AlarmService;
 import gwangju.ssafy.backend.global.exception.EmailException;
 import gwangju.ssafy.backend.global.exception.GlobalError;
 import jakarta.mail.Message;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,90 +15,70 @@ import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
+@RequiredArgsConstructor
 @Service
 public class EmailServiceImpl implements EmailService {
-    @Autowired
-    JavaMailSender emailSender;
 
-    private static final String ePw = createKey();
+	private final AlarmService alarmService;
 
-    private MimeMessage createMessage(String to, boolean signupCheck) throws Exception {
-        System.out.println("보내는 대상: " + to);
-        System.out.println("인증 번호: " + ePw);
-        MimeMessage message = emailSender.createMimeMessage();
+	private String createMessage(String to, boolean signupCheck, String key) {
 
-        message.addRecipients(Message.RecipientType.TO, to);    // 보내는 대상
-        if(signupCheck) {
-            message.setSubject("이메일 인증코드 입니다."); // 제목
-        }
-        else {
-            message.setSubject("임시 비밀번호 입니다.");
-        }
+		StringBuffer sb = new StringBuffer();
 
-        String msgg = "";
+		sb.append("아래 코드를 복사해 입력해주세요.").append("\n")
+			.append("감사합니다.").append("\n").append("\n");
 
-        msgg+= "<div style='margin:20px;'>";
-        if(signupCheck) {
-            msgg+= "<h1> SSoC 회원가입 이메일 인증번호</h1>";
-        }
-        else {
-            msgg+= "<h1> SSoC 임시 비밀번호 재발급</h1>";
-        }
-        msgg+= "<br>";
-        msgg+= "<p>아래 코드를 복사해 입력해주세요.<p>";
-        msgg+= "<br>";
-        msgg+= "<p>감사합니다.<p>";
-        msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        if(signupCheck) {
-            msgg+= "<h3 style='color:blue;'>회원가입 메일 인증 코드입니다.</h3>";
-        }
-        else {
-            msgg+= "<h3 style='color:blue;'>임시 비밀번호 재발급 코드입니다.</h3>";
-        }
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= ePw+"</strong><div><br/> ";
-        msgg+= "</div>";
+		if (signupCheck) {
+			sb.append("회원가입 메일 인증 코드 : ");
+		} else {
+			sb.append("임시 비밀번호 재발급 코드 : ");
+		}
 
-        message.setText(msgg, "utf-8", "html"); // 안에 들어갈 내용 세팅
-        message.setFrom(new InternetAddress("donggeun3484@gmail.com", "SSoc"));
-        return message;
-    }
+		sb.append(key).append("\n");
 
-    @Override
-    public MailCodeDto sendSimpleMessage(String to, boolean signupCheck) throws Exception {
-        MimeMessage message = createMessage(to, signupCheck);
-        try {
-            emailSender.send(message);
-        }
-        catch(MailException e) {
-            throw new EmailException(GlobalError.NOT_SEND_EMAIL);
-        }
+		return sb.toString();
+	}
 
-        return MailCodeDto.builder().userEmail(to).emailCode(ePw).build();
-    }
+	@Override
+	public MailCodeDto sendSimpleMessage(String to, boolean signupCheck) throws Exception {
 
-    public static String createKey() {
-        StringBuffer key = new StringBuffer();
-        Random rnd = new Random();
+		String ePw = createKey();
+		
+		try {
+			if (signupCheck) {
+				alarmService.sendMailAlarm("이메일 인증코드 입니다.", createMessage(to, signupCheck, ePw), to,
+					"alarm");
+			} else {
+				alarmService.sendMailAlarm("임시 비밀번호 입니다.", createMessage(to, signupCheck, ePw), to,
+					"alarm");
+			}
+		} catch (MailException e) {
+			throw new EmailException(GlobalError.NOT_SEND_EMAIL);
+		}
 
-        // 인증코드 8자리로
-        for(int i=0; i<8; i++) {
-            int index = rnd.nextInt(3); // 0 ~ 2 까지 랜덤
+		return MailCodeDto.builder().userEmail(to).emailCode(ePw).build();
+	}
 
-            switch (index) {
-                case 0:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                    break;
-                case 1:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    break;
-                case 2:
-                    key.append((rnd.nextInt(10)));
-                    break;
-            }
-        }
-        return key.toString();
-    }
+	public static String createKey() {
+		StringBuffer key = new StringBuffer();
+		Random rnd = new Random();
+
+		// 인증코드 8자리로
+		for (int i = 0; i < 8; i++) {
+			int index = rnd.nextInt(3); // 0 ~ 2 까지 랜덤
+
+			switch (index) {
+				case 0:
+					key.append((char) ((int) (rnd.nextInt(26)) + 97));
+					break;
+				case 1:
+					key.append((char) ((int) (rnd.nextInt(26)) + 65));
+					break;
+				case 2:
+					key.append((rnd.nextInt(10)));
+					break;
+			}
+		}
+		return key.toString();
+	}
 }
