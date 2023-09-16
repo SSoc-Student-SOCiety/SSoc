fc8d8c2a5bf394d0bcf4bbfc9c7ea76cbbdfc086
Dohun Lee <nestour95@gmail.com>
2023-09-16 09:54:29 +0900

    chore: 자동 배포 테스트

--- Backend/src/main/java/gwangju/ssafy/backend/global/infra/batch/dto/DailyTransactionStatisticsBatchDto.java
+++ Backend/src/main/java/gwangju/ssafy/backend/global/infra/batch/dto/DailyTransactionStatisticsBatchDto.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DailyTransactionStatisticsBatchDto {
-	// groupAccountId
+	//t
 	private Long id;
 	private Long totalWithdrawal;
 	private Long totalDeposit;
