f7fef8636e3a2003484e8765bbedefbbee848b31
동긔 <tlsehdrms124@naver.com>
2023-09-17 06:54:14 +0900

    hotfix: 최종 수정

--- Backend/src/main/java/gwangju/ssafy/backend/domain/reservation/entity/enums/ReservationApproveStatus.java
+++ Backend/src/main/java/gwangju/ssafy/backend/domain/reservation/entity/enums/ReservationApproveStatus.java
@@ -7,6 +7,16 @@ public enum ReservationApproveStatus {
     // 승인, 거절, 미확인 상태
     ACCEPT, REJECT, NOTCONFIRM;
 
+    @JsonCreator
+    public static ReservationApproveStatus from(String value) {
+        for(ReservationApproveStatus reservationApproveStatus: ReservationApproveStatus.values()) {
+            if(reservationApproveStatus.name().equals(value)) {
+                return reservationApproveStatus;
+            }
+        }
+        return null;
+    }
+
     @JsonValue
     public String getValue() {
         return this.name();
